295eec53f7c78872d40e7637bde24699fc154ae8
Tukain <dfern3321+codeberg@gmail.com>
2026-01-16 12:51:34 -0300

    Updates

--- modules/menu.js
+++ modules/menu.js
@@ -12,12 +12,6 @@ export function menu() {
   create_priv_page("mobile_menu", "Menu");
   create_element("ul", "menu_list", "mobile_menu");
 
-  create_element("li", "menu_item", "menu_list");
-  create_element("a", "home_menu", "menu_item");
-  set_attribute("home_menu", "href", "#home");
-  set_attribute("home_menu", "class", "blog_entry");
-  add_text("home_menu", "Início");
-
   let i = 0;
   for (let page of page_list) {
     create_element("li", `menu_item_${i}`, "menu_list");
