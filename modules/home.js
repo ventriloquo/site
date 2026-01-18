2a79dcbc92359c60c604f37726e74fb9ac7f5fef
Tukain <tucano@firemail.cc>
2026-01-17 21:50:23 -0300

    TODO: Últimos 3 posts

--- modules/home.js
+++ modules/home.js
@@ -1,6 +1,8 @@
 "use strict";
 
-import { create_priv_page } from "../index.js";
+import {
+  create_priv_page, create_element, slug } from "../index.js";
+import { posts } from "./db/posts.js";
 
 export function home() {
   create_priv_page(
@@ -16,5 +18,19 @@ que eu achar pertinente.
 <blockquote>
   <p>"The strength of JavaScript is that you can do anything. The weakness is that you will." - Reg Braithwaite</p>
 </blockquote>
+
+<h2>Últimos posts</h2>
 `);
+
+  // for (let i = 0; i < 2; i++) {
+  //   const id = `${slug(posts[i].date)}_${slug(posts[i].title)}`;
+  //   const id_link = `${slug(posts[i].date)}_entry`;
+
+  //   create_element("li", `${id}`, "entry_list");
+  //   create_element("a", `${id_link}`, `${id}`);
+
+  //   set_attribute(`${id_link}`, "href", `#${id}_page`);
+  //   set_attribute(`${id_link}`, "class", "blog_entry");
+  //   add_text(`${id_link}`, `${posts[i].date} - ${posts[i].title}`);
+  // }
 }
