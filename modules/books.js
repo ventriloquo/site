4e734770cf6a9751f830b6fbf528f0767c3e9c0c
Tukain <tucano@firemail.cc>
2026-01-30 21:28:00 -0300

    Updates

--- modules/books.js
+++ modules/books.js
@@ -9,11 +9,35 @@ import {
 import { bookshelf } from "./db/books.js";
 
 export function books() {
-  create_page("bookshelf", "Lista de Leitura", tag("p", {}, "Essa é a minha coleção de livros e mangás"));
+  let r = 0, s = 0, n = 0;
+  for (const b of bookshelf) {
+    if (b.progress.current === b.progress.maximum) r++;
+    if (Number(b.progress.current) > 0 && Number(b.progress.current) < Number(b.progress.maximum)) s++;
+    if (Number(b.progress.current) === 0) n++;
+  }
+  const t = bookshelf.length;
 
-  document.getElementById("bookshelf").appendChild(
-    tag("div", {"id":"shelf", "style":"display: flex; flex-wrap: wrap; justify-content: center"})
+  create_page(
+    "bookshelf", "Lista de Leitura",
+    tag("div", {},
+      tag("p", {}, "Essa é a minha coleção de livros e mangás"),
+      tag("h3", {}, "Status"),
+      tag("table", {},
+        tag("tbody", {},
+          tag("tr", {},
+            tag("th", {}, "Livros lidos"), tag("th", {}, "Livros sendo lidos"), tag("th", {}, "Livros não lidos")
+          ),
+          tag("tr", {},
+            tag("td", {}, `${r}`), tag("td", {}, `${s}`), tag("td", {}, `${n}`)
+          ),
+        )),
+      tag("progress", {"value":`${r}`, "max":`${t}`}),
+      tag("p", {"style":"margin: 0; text-align: center"}, `Li <span style='color: rgba(var(--ac-1), 1)'>${r}</span> de <span style='color: rgba(var(--ac-0), 1)'>${t}</span> livros da minha coleção.`),
+      tag("h3", {}, "Coleção"),
+      tag("div", {"id":"shelf", "style":"display: flex; flex-wrap: wrap; justify-content: center"})
+    )
   );
+
   for (const book of bookshelf) {
     document.getElementById("shelf").appendChild(
       tag("div", {"style":"margin: 10px"},
